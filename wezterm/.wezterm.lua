fdef110278946c5185c1946f37003ac17b3f95b1
Felipe Flores <felipebruckmann@hotmail.com.br>
2024-08-11 17:04:47 -0400

    lint: Removing comments

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,16 +1,14 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
 
--- For example, changing the color scheme:
+--- Theme config
+
 config.color_scheme = 'Catppuccin Mocha'
 config.font = wezterm.font 'JetBrainsMono NF'
 config.warn_about_missing_glyphs = false
 config.enable_tab_bar = false
 
--- and finally, return the configuration to wezterm
 return config
